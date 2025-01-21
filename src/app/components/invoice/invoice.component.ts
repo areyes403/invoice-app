f38b347c90f46612767a43fd6318ab4598692cee
areyes403 <abrahamreyes321@hotmail.com>
2025-01-21 15:13:20 -0600

    added some components

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
+import { ListItemsComponent } from '../list-items/list-items.component';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { RowItemComponent } from '../row-item/row-item.component';
+import { TotalComponent } from "../total/total.component";
 
 @Component({
   selector: 'app-invoice',
-  imports: [],
+  imports: [ClientViewComponent, InvoiceViewComponent, ListItemsComponent, CompanyViewComponent, TotalComponent],
   templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit {
@@ -16,5 +22,9 @@ export class InvoiceComponent implements OnInit {
   }
   ngOnInit(): void {
     this.invoice=this.service.getInvoice()
+  }
+
+  removeItem(id:number){
+    this.invoice=this.service.remove(id)
   }
 }
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -12,6 +12,23 @@ export class InvoiceService {
   constructor() { }
 
   getInvoice(): Invoice {
-    return this.invoice;
+    const total = this.calculateTotal();
+    return {... this.invoice, total};
+  }
+
+  private calculateTotal():number{
+  /*  let total=0;
+    this.invoice.items.forEach(item=>{
+      total += item.total();
+    });
+    return total; */
+    return this.invoice.items.reduce((total,item)=> total + (item.price*item.quantity), 0);
+ 
+  }
+
+  remove(id:number):Invoice{
+    this.invoice.items=this.invoice.items.filter(item=>item.id!=id)
+    const total=this.calculateTotal();
+    return {... this.invoice,total};
   }
 }
