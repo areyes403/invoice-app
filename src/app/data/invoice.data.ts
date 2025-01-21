0f955fc99f033f40139e546fb81d05d75f707954
areyes403 <abrahamreyes321@hotmail.com>
2025-01-20 18:27:38 -0600

    invoice created

---
+++ src/app/data/invoice.data.ts
@@ -0,0 +1,40 @@
+import { Invoice } from "../models/invoice";
+
+export const invoiceData:Invoice = {
+    id: 1,
+    name: 'Componente de PC',
+    client:{
+        name: 'Andres',
+        lastname: 'Doe',
+        address:{
+            country: 'USA',
+            city: 'Los Angeles',
+            street: 'One Street',
+            number: 15
+        }
+    },
+    company: {
+        name: 'New Age',
+        fisicalNumber:123123
+    },
+    items:[
+        {
+            id:1,
+            product: 'Cpu Intel i9',
+            price: 599,
+            quantity:1
+        },
+        {
+            id:2,
+            product: 'Teclado',
+            price: 299,
+            quantity:2
+        },
+        {
+            id:3,
+            product: 'Monitor',
+            price: 800,
+            quantity:3
+        }
+    ]
+}
